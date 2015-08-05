408ecba29d8067c89ee6ce513226107642f96eea
J. Aaron Eaton <aaron@channeleaton.com>
2015-08-05 11:41:22 -0500

    Added subscribe to course

--- src/Service/CourseService.php
+++ src/Service/CourseService.php
@@ -5,11 +5,25 @@ namespace AwesomeMotive\ConvertKit\Service;
 class CourseService extends AbstractService {
 
 	/**
-	 * @return array
+	 * @return \stdClass
 	 */
 	public function all() {
 
 		return $this->client->request( 'courses' );
+
+	}
+
+	/**
+	 * @param int $course_id
+	 * @param array $data
+	 *
+	 * @return \stdClass
+	 */
+	public function subscribe( $course_id, $data ) {
+
+		$path = 'courses/' . $course_id . '/subscribe';
+
+		return $this->client->request( $path, 'post', $data );
 
 	}
 
